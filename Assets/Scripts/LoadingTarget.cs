b1d0d82074fa3b54e9d1cd7b4e8fa848faf125f4
Luca Neri <lucasmodzita@gmail.com>
2017-01-16 23:34:43 +0100

    - Project

---
+++ Assets/Scripts/LoadingTarget.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadingTarget : MonoBehaviour
+{
+    public Image radialSlider;
+    public float secondsToFill;
+
+    public byte numberOfTarget;
+
+    internal Action<byte> targetEvent;
+
+    public void StartTimer()
+    {
+        StopAllCoroutines();
+        StartCoroutine(IncreaseFillAmountCO());
+    }
+
+    public void StopTimer()
+    {
+        StopAllCoroutines();
+        StartCoroutine(DecreaseFillAmountCO());
+    }
+    
+    private IEnumerator IncreaseFillAmountCO()
+    {
+        while (radialSlider.fillAmount < 1)
+        {
+            radialSlider.fillAmount += (Time.deltaTime / secondsToFill);
+            yield return null;
+        }
+        
+        targetEvent(numberOfTarget);
+    }
+
+    private IEnumerator DecreaseFillAmountCO()
+    {
+        while (radialSlider.fillAmount > 0)
+        {
+            radialSlider.fillAmount -= (Time.deltaTime * 2);
+            yield return null;
+        }
+    }
+}
